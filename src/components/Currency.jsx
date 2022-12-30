daa196899c5b563077d71b9ea60b10009af157bb
Francis Rodriguez <francisngg@gmail.com>
2022-12-30 12:06:39 -0400

    Linted Currency.jsx

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,40 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import Select from 'react-select'
-import axios from "axios";
-import { urls } from "./constants.js";
-import './Currency.css'
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+import axios from 'axios';
+import { urls } from './constants';
+import './Currency.css';
 
-export const Currency = () => {
-    const [currencies, setCurrencies] = useState([]);
-    const getCurrency = (value = 'https://pydolarlibre.francis.center/api/v1/argcurrency/json') => {
-       axios
-           .get(value)
-           .then((response) => setCurrencies(response.data))
-    }
+export function Currency() {
+  const [currencies, setCurrencies] = useState([]);
+  const getCurrency = (value = 'https://pydolarlibre.francis.center/api/v1/argcurrency/json') => {
+    axios
+      .get(value)
+      .then((response) => setCurrencies(response.data));
+  };
 
-    useEffect(() => {
-        getCurrency()
-    }, [])
+  useEffect(() => {
+    getCurrency();
+  }, []);
 
-    const handleSelectChange = ( value ) => {
-        getCurrency(value.value)
-    }
+  const handleSelectChange = (value) => {
+    getCurrency(value.value);
+  };
 
-return (
-    <>
-        <div className="Currency-container">
-            <Select
-                defaultValue = { { label: 'ARG', value: 'https://pydolarlibre.francis.center/api/v1/argcurrency/json' } }
-                options = { urls }
-                onChange = { handleSelectChange }
-            />
-            <div className="Currencies">
-                <p>BUY: {currencies.Compra}$</p>
-                <p>SELL: {currencies.Venta}$</p>
-            </div>
-        </div>
-
-
-    </>
-    )
+  return (
+    <div className="Currency-container">
+      <Select
+        defaultValue={{ label: 'ARG', value: 'https://pydolarlibre.francis.center/api/v1/argcurrency/json' }}
+        options={urls}
+        onChange={handleSelectChange}
+      />
+      <div className="Currencies">
+        <p>
+          BUY:
+          {currencies.Compra}
+          $
+        </p>
+        <p>
+          SELL:
+          {currencies.Venta}
+          $
+        </p>
+      </div>
+    </div>
+  );
 }
