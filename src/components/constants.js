e2d4f67b13f7552ba991f07fb5d2496f8745ed4a
Francis Rodriguez <francisngg@gmail.com>
2022-12-30 11:53:22 -0400

    Added eslint

---
+++ src/components/constants.js
@@ -0,0 +1,5 @@
+export const urls = [
+    {label: 'ARG', value: 'https://pydolarlibre.francis.center/api/v1/argcurrency/json'},
+    {label: 'COL', value: 'https://pydolarlibre.francis.center/api/v1/cop/json'},
+    {label: 'BRA', value: 'https://pydolarlibre.francis.center/api/v1/brl/json'},
+]
