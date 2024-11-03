0370bd024b02ef6cf53ba9a6c3d8f8992ba4fa7c
MoisesSavelli <133018474+MoisesSavelli@users.noreply.github.com>
2024-11-03 16:11:55 -0300

    Adding Chile currency
    Adding Chilean Peso currency

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,6 @@
 export const urls = [
   { label: 'Argentina', value: 'https://pydolarlibre.francis.center/api/v1/ARG/json' },
+  { label: 'Chile', value: 'https://pydolarlibre.francis.center/api/v1/CLP/json' },
   { label: 'Colombia', value: 'https://pydolarlibre.francis.center/api/v1/COP/json' },
   { label: 'Brasil', value: 'https://pydolarlibre.francis.center/api/v1/BRL/json' },
   { label: 'Venezuela', value: 'https://pydolarlibre.francis.center/api/v1/ven/json' },
