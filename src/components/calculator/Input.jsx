f8d6f33fa0a9233df42f1bcb7abc6ff1742bb46b
Moisés Savelli <84404250+mxavelli@users.noreply.github.com>
2025-09-19 18:14:45 -0300

    Update Input.jsx

--- src/components/calculator/Input.jsx
+++ src/components/calculator/Input.jsx
@@ -3,6 +3,15 @@ import { NumericFormat } from 'react-number-format';
 export const Input = ({ name, onChange, value }) => (
   <div className="mt-3.5 mb-5 flex flex-col">
     <label htmlFor={name} className="font-bold ml-1 mb-0.5">{name}</label>
-    <NumericFormat className="p-2 rounded-lg" value={value} onValueChange={onChange} thousandSeparator="," allowNegative={false} decimalScale={2} prefix="$" />
+    <NumericFormat
+      className="p-2 rounded-lg"
+      value={value}
+      onValueChange={onChange}
+      thousandSeparator=","
+      allowNegative={false}
+      decimalScale={2}
+      prefix="$"
+      onFocus={() => onChange({ floatValue: null, value: '' })}
+    />
   </div>
 );
